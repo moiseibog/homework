8c36d4a02f6f839c9d832fb6c09f941fe28af280
Moisei_bog <moiseenkokirill0@gmail.com>
2022-10-01 12:42:09 +0300

    Задача к семинару 4.

--- test_1.cpp
+++ test_1.cpp
@@ -2,6 +2,29 @@
 using namespace std;
 
 int main() {
-    cout << "Hello, World!";
-    cout << "kakaska";
+    cout << "Введите размер множества: ";
+    int size;
+    cin >> size;
+    const int s = size;
+    cout << "Введите элементы множества: ";
+    int *num = new int[size];
+    for (int i = 0; i < size; i++) {
+        cin >> num[i];
+    }
+    cout << "Введите заданное число: ";
+    int gnum;
+    cin >> gnum;
+    cout << "Найденные подмножества:" << endl; //В рамках задачи ищем подмножества только из двух элементов.
+    int count = 0;
+    for (int i = 0; i < size - 1; i++) {
+        for (int j = i + 1; j < size; j++) {
+            if ((num[i] + num[j]) == gnum) {
+                count++;
+                cout << num[i] << " " << num[j] << endl;
+            }
+        }
+    }
+    if (count == 0) {
+        cout << "Подмножеств из двух элементов нет.";
+    }
 }
