cfc1167d3b00285f49f53513c2d1ad8fd1cc35ce
Moisei_bog <moiseenkokirill0@gmail.com>
2022-10-06 01:57:31 +0300

    tested

--- test_1.cpp
+++ test_1.cpp
@@ -1,30 +1,15 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-    cout << "Введите размер множества: ";
-    int size;
-    cin >> size;
-    const int s = size;
-    cout << "Введите элементы множества: ";
-    int *num = new int[size];
-    for (int i = 0; i < size; i++) {
-        cin >> num[i];
+double deg(double x, int k) {
+    double X;
+    X = x;
+    for (int i = 2; i <= k; i++) {
+        X *= x;
     }
-    cout << "Введите заданное число: ";
-    int gnum;
-    cin >> gnum;
-    cout << "Найденные подмножества:" << endl; //В рамках задачи ищем подмножества только из двух элементов.
-    int count = 0;
-    for (int i = 0; i < size - 1; i++) {
-        for (int j = i + 1; j < size; j++) {
-            if ((num[i] + num[j]) == gnum) {
-                count++;
-                cout << num[i] << " " << num[j] << endl;
-            }
-        }
-    }
-    if (count == 0) {
-        cout << "Подмножеств из двух элементов нет.";
-    }
+    return X;
+}
+
+int main(){
+    cout << deg(0.25, 3);
 }
